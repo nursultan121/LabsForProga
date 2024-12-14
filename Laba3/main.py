b4bc2c279281928bbaeb83fc5974c812f20c7e65
BestStatham <pugachev.yr@gmail.com>
2024-12-14 16:35:08 +0300

    гит привет от детей

--- Laba3/main.py
+++ Laba3/main.py
@@ -1,1 +1,15 @@
-print('hello mario balls')
+import re
+import requests
+
+PATTERN = (r'\b(?:(?:[0-9A-F]{2}[:]){5}(?:[0-9A-F]){2})\b|'
+           r'\b(?:(?:[0-9A-F]{2}[-]){5}(?:[0-9A-F]){2})\b')
+LINK = 'https://en.wikipedia.org/wiki/MAC_address'
+
+
+def mac_return_valid(mac):
+    return re.findall(PATTERN, mac, flags=re.I)
+
+def mac_from_file(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        return max_return_valid(file.read())
+    return None
